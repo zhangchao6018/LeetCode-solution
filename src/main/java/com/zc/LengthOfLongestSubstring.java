43e894809b30f8b1bfb629891b7343978653e52e
“zhangchao22 <18910146715@163.com>
2020-03-31 20:06:11 +0800

    日期校验新方法-正则

--- src/main/java/com/zc/LengthOfLongestSubstring.java
+++ src/main/java/com/zc/LengthOfLongestSubstring.java
@@ -1,7 +1,9 @@
 package com.zc;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @Description:
@@ -16,21 +18,39 @@ import java.util.List;
 
 
 public class LengthOfLongestSubstring {
+    //todo 当传入""  错误 ->预期 1 结果:0
     public static int lengthOfLongestSubstring(String s) {
-
+        if ("".equals(s)) {
+            return 0;
+        }
+        String trim = s.trim();
+        if ("".equals(trim)) {
+            return 1;
+        }
+        int result = 1;
         List<String> lis = Arrays.asList(s.split(""));
-        for (String li : lis) {
-
-
-
+        Set set = new HashSet();
+        for (int i = 0 ;  i< lis.size() ; i++) {
+            String s1 = lis.get(i);
+            if (!set.contains(s1)) {
+                boolean add = set.add(s1);
+            }else {
+                //重复了->统计size
+                int size = set.size();
+                result = result>size?result:size;
+                //清空set
+                set.clear();
+                set.add(s1);
+            }
         }
 
-        return 1;
+        return result;
 
     }
 
     public static void main(String[] args) {
-        String s = "hhjajayyiqww";
-        lengthOfLongestSubstring(s);
+//        String s = "hhjajayyiqww";
+        String s = "";
+        System.out.println(lengthOfLongestSubstring(s));
     }
 }
