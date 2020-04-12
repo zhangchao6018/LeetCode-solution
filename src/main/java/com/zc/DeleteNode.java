90dc1835fec76f610f4e593ef3e66eb53b091d1a
zhangchao <18910146715@163.com>
2020-04-12 16:14:59 +0800

    递归-删除链表元素

--- src/main/java/com/zc/DeleteNode.java
+++ src/main/java/com/zc/DeleteNode.java
@@ -16,12 +16,12 @@ package com.zc;
  **/
 public class DeleteNode {
     public static ListNode deleteNode(ListNode head, int val) {
-        return m3(head, val);
+        return m5(head, val);
     }
 
     /**
      * 有个漏洞 如果头节点之后的节点也是待删除元素 该方法错误
-     * @see     DeleteNode.m2
+     * @see     DeleteNode m2
      * @param head
      * @param val
      * @return
@@ -94,6 +94,35 @@ public class DeleteNode {
         return dummyHead.next;
     }
 
+    /**
+     * 递归的方式解决
+     * @param head
+     * @param val
+     * @return
+     */
+    private static ListNode m4(ListNode head, int val) {
+        if (head==null)
+            return null;
+        if (head.val == val)
+            head = head.next;
+        else
+            return head;
+
+        return m4(head,val);
+    }
+    /**
+     * 简洁的递归(跟m4是等效的)
+     * @param head
+     * @param val
+     * @return
+     */
+    private static ListNode m5(ListNode head, int val) {
+        if (head==null)
+            return null;
+        head.next = m5(head.next,val);
+        return head.val==val?head.next:head;
+    }
+
     public static void main(String[] args) {
 //        ListNode node2 = new ListNode(4);
 //        ListNode node =  new ListNode(5);
@@ -101,18 +130,22 @@ public class DeleteNode {
 //        ListNode node4 = new ListNode(2);
 //        ListNode node5 = new ListNode(1);
 //        ListNode node6 = new ListNode(3);
-        ListNode node =  new ListNode(5);
-        ListNode node2 = new ListNode(5);
-        ListNode node3 = new ListNode(5);
-        ListNode node4 = new ListNode(5);
-        ListNode node5 = new ListNode(5);
-        ListNode node6 = new ListNode(5);
-        node.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        ListNode listNode = deleteNode(node, 5);
-        System.out.println();
+
+//        ListNode node =  new ListNode(5);
+//        ListNode node2 = new ListNode(5);
+//        ListNode node3 = new ListNode(5);
+//        ListNode node4 = new ListNode(5);
+//        ListNode node5 = new ListNode(5);
+//        ListNode node6 = new ListNode(5);
+//        node.next = node2;
+//        node2.next = node3;
+//        node3.next = node4;
+//        node4.next = node5;
+//        node5.next = node6;
+
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        ListNode listNode = deleteNode(head, 5);
+        System.out.println(listNode);
     }
 }
--- src/main/java/com/zc/ListNode.java
+++ src/main/java/com/zc/ListNode.java
@@ -5,11 +5,40 @@ public class ListNode {
       ListNode next;
       ListNode(int x) { val = x; }
 
+//    @Override
+//    public String toString() {
+//        return "ListNode{" +
+//                "val=" + val +
+//                ", next=" + next +
+//                '}';
+//    }
+
+    // 链表节点的构造函数
+    // 使用arr为参数，创建一个链表，当前的ListNode为链表头结点
+    public ListNode(int[] arr){
+
+        if(arr == null || arr.length == 0)
+            throw new IllegalArgumentException("arr can not be empty");
+
+        this.val = arr[0];
+        ListNode cur = this;
+        for(int i = 1 ; i < arr.length ; i ++){
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+    }
+
+    // 以当前节点为头结点的链表信息字符串
     @Override
-    public String toString() {
-        return "ListNode{" +
-                "val=" + val +
-                ", next=" + next +
-                '}';
+    public String toString(){
+
+        StringBuilder s = new StringBuilder();
+        ListNode cur = this;
+        while(cur != null){
+            s.append(cur.val + "->");
+            cur = cur.next;
+        }
+        s.append("NULL");
+        return s.toString();
     }
 }
