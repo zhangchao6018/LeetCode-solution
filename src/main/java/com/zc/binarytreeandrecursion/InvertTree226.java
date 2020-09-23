94b8d206c13deb342fb1c3cbf7783b2d94e38158
zhangchao <18910146715@163.com>
2020-09-24 00:32:08 +0800

    InvertTree

---
+++ src/main/java/com/zc/binarytreeandrecursion/InvertTree226.java
@@ -0,0 +1,45 @@
+package com.zc.binarytreeandrecursion;
+
+/**
+ * 描述:
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ *
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/invert-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author: zhangchao
+ **/
+public class InvertTree226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        return root;
+    }
+    public static void main(String[] args) {
+
+    }
+}
